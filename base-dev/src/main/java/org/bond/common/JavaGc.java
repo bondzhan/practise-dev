8602e2fdc6c464b5e0938ab7a888099a249ea351
bondzhan <ahead_zhan@163.com>
2018-01-03 14:12:30 +0800

    ignore 一些文件

--- base-dev/src/main/java/org/bond/common/JavaGc.java
+++ base-dev/src/main/java/org/bond/common/JavaGc.java
@@ -24,7 +24,7 @@ public class JavaGc {
 	 */
 	public static void main(String[] args) {
 		byte[] b1, b2, b3, b4;// 定义变量
-		b1 = new byte[1650 * 1024];// 分配 1MB 堆空间，考察堆空间的使用情况
+		b1 = new byte[1024 * 1024];// 分配 1MB 堆空间，考察堆空间的使用情况
 		b2 = new byte[1024 * 1024];
 		b3 = new byte[1024 * 1024];
 		b4 = new byte[1024 * 1024];
